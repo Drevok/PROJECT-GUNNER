cd1d1fe4a792892d687561946a8c63b15fb8ebc8
Drevok <theoartous@hotmail.fr>
2020-10-15 17:41:28 +0200

    Started save script

---
+++ Assets/Scripts/DataHandler.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using  System.IO;
+using Application = UnityEngine.Application;
+using System;
+
+public static class DataHandler
+{
+    public static void Save<T>(UnityDirectory unityDirectory, T data, string fileName)
+    {
+        ChackAndCreateDirectory(GetDirectory)
+    }
+    
+    
+}
