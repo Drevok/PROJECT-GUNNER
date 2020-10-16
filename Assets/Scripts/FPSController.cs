11dc184b859f6ff9f4c3ee141482388649b6a00b
Drevok <theoartous@hotmail.fr>
2020-10-16 15:08:54 +0200

    Only one error and my bullets would be loaded

--- Assets/Scripts/FPSController.cs
+++ Assets/Scripts/FPSController.cs
@@ -4,110 +4,116 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-public class FPSController : MonoBehaviour
+namespace Scripts
 {
-    public Camera fpsCam;
-    
-    public InputAction m_moveAction;
 
-    public InputAction m_lookAction;
 
-    public InputAction m_shootAction;
 
-    [SerializeField] private float m_rotateSpeed;
-
-    [SerializeField] 
-    private float m_moveSpeed;
-    private Vector3 m_rotation;
-
-    [SerializeField] private GameObject m_bulletPrefab;
-
-    [SerializeField] private Transform m_spawnPoint;
-
-    public List<GameObject> bullets;
-
-    private void Awake()
+    public class FPSController : MonoBehaviour
     {
-        m_shootAction.performed += _ctx => { Shoot(); };
-    }
-    
-    // Start is called before the first frame update
-    void Start()
-    {
+        public Camera fpsCam;
+
+        public InputAction m_moveAction;
+
+        public InputAction m_lookAction;
+
+        public InputAction m_shootAction;
+
+        [SerializeField] private float m_rotateSpeed;
+
+        [SerializeField] private float m_moveSpeed;
+        private Vector3 m_rotation;
+
+        [SerializeField] private GameObject m_bulletPrefab;
+
+        [SerializeField] private Transform m_spawnPoint;
         
-    }
 
-    private void OnEnable()
-    {
-        m_lookAction.Enable();
-        m_moveAction.Enable();
-        m_shootAction.Enable();
-    }
-
-    private void OnDisable()
-    {
-        m_lookAction.Disable();
-        m_moveAction.Disable();
-        m_shootAction.Disable();
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        var look = m_lookAction.ReadValue<Vector2>();
-        var move = m_moveAction.ReadValue<Vector2>();
-
-        Look(look);
-        Move(move);
-    }
-
-    private void Move(Vector2 _moveVector)
-    {
-        if (_moveVector.sqrMagnitude < 0.01) return;
-
-        var moveSpeedPerFrame = m_moveSpeed * Time.deltaTime;
-        var move = Quaternion.Euler(0, transform.eulerAngles.y, 0) * new Vector3(_moveVector.x, 0, _moveVector.y);
-        transform.position += move * moveSpeedPerFrame;
-    }
-
-    private void Look(Vector2 _rotate)
-    {
-        if (_rotate.sqrMagnitude < 0.01) return;
-
-        var rotateSpeedPerFrame = m_rotateSpeed * Time.deltaTime;
-        m_rotation.y += _rotate.x * rotateSpeedPerFrame;
-        m_rotation.x = Mathf.Clamp(m_rotation.x - _rotate.y * rotateSpeedPerFrame, -89, 89);
-        transform.localEulerAngles = m_rotation;
-    }
-    
-    private void Shoot()
-    {
-        /*Ray ray = fpsCam.ViewportPointToRay((new Vector3(0.5f, 0.5f, 0)));
-        
-        
-        RaycastHit hit;
-        Vector3 targetPoint;
-        if (Physics.Raycast(ray, out hit))
-            targetPoint = hit.point;
-        else
+        private void Awake()
         {
-            targetPoint = ray.GetPoint(75);
+            m_shootAction.performed += _ctx => { Shoot(); };
+        }
+        
+
+        private void OnEnable()
+        {
+            m_lookAction.Enable();
+            m_moveAction.Enable();
+            m_shootAction.Enable();
         }
 
-        Vector3 directionWithoutSpread = targetPoint;
-        
-        GameObject currentBullet = Instantiate(m_bulletPrefab, m_spawnPoint.position, Quaternion.identity);
-        currentBullet.transform.forward = directionWithoutSpread.normalized;
-        
-        currentBullet.GetComponent<Rigidbody>().AddForce(directionWithoutSpread.normalized * 50, ForceMode.Impulse);*/
-        GameObject Ammo = Instantiate(m_bulletPrefab, m_spawnPoint.position, Quaternion.identity);
-        Ammo.GetComponent<Rigidbody>().AddForce(Vector3.forward);
-        bullets.Add(Ammo);
-    }
+        private void OnDisable()
+        {
+            m_lookAction.Disable();
+            m_moveAction.Disable();
+            m_shootAction.Disable();
+        }
 
-    void LoadBullets()
-    {
-        bullets = DataHandler.Load<bullets>()
+        // Update is called once per frame
+        void Update()
+        {
+            var look = m_lookAction.ReadValue<Vector2>();
+            var move = m_moveAction.ReadValue<Vector2>();
+
+            Look(look);
+            Move(move);
+        }
+
+        private void Move(Vector2 _moveVector)
+        {
+            if (_moveVector.sqrMagnitude < 0.01) return;
+
+            var moveSpeedPerFrame = m_moveSpeed * Time.deltaTime;
+            var move = Quaternion.Euler(0, transform.eulerAngles.y, 0) * new Vector3(_moveVector.x, 0, _moveVector.y);
+            transform.position += move * moveSpeedPerFrame;
+        }
+
+        private void Look(Vector2 _rotate)
+        {
+            if (_rotate.sqrMagnitude < 0.01) return;
+
+            var rotateSpeedPerFrame = m_rotateSpeed * Time.deltaTime;
+            m_rotation.y += _rotate.x * rotateSpeedPerFrame;
+            m_rotation.x = Mathf.Clamp(m_rotation.x - _rotate.y * rotateSpeedPerFrame, -89, 89);
+            transform.localEulerAngles = m_rotation;
+        }
+
+        private void Shoot()
+        {
+            /*Ray ray = fpsCam.ViewportPointToRay((new Vector3(0.5f, 0.5f, 0)));
+            
+            
+            RaycastHit hit;
+            Vector3 targetPoint;
+            if (Physics.Raycast(ray, out hit))
+                targetPoint = hit.point;
+            else
+            {
+                targetPoint = ray.GetPoint(75);
+            }
+    
+            Vector3 directionWithoutSpread = targetPoint;
+            
+            GameObject currentBullet = Instantiate(m_bulletPrefab, m_spawnPoint.position, Quaternion.identity);
+            currentBullet.transform.forward = directionWithoutSpread.normalized;
+            
+            currentBullet.GetComponent<Rigidbody>().AddForce(directionWithoutSpread.normalized * 50, ForceMode.Impulse);*/
+            GameObject Ammo = Instantiate(m_bulletPrefab, m_spawnPoint.position, Quaternion.identity);
+            Ammo.GetComponent<Rigidbody>().AddForce(Vector3.forward);
+            Bullets.bullets.Add(Ammo);
+        }
+
+        void LoadBullets()
+        {
+            Bullets.bullets = DataHandler.Load<Bullets>(DataHandler.UnityDirectory.StreamingAsset, "bullet");
+        }
+
     }
     
+    [Serializable]
+    public static class Bullets
+    {
+        public static List<GameObject> bullets;
+    }
 }
+
